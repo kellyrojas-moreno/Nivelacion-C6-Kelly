fdb842b77c81acf914eaa4feed87f3b29130b1d5
Kelly Rojas <kellyyanethrojasmoreno@gmail.com>
2025-12-03 10:45:52 -0500

    subir cambios a repositorio de funcion

--- Ejercicio11.py
+++ Ejercicio11.py
@@ -1,9 +1,12 @@
 
-contraseña_sistema  = "admin123"
+def validacion_contraseña():
+    contraseña_sistema  = "admin123"
 
-Contraseña_usuario = input("ingrese contraseña")
+    Contraseña_usuario = input("ingrese contraseña")
 
-if contraseña_sistema == Contraseña_usuario:
-    print("Contraseña correcta")
-else:
-    print("contraseña incorrecta")        
+    if contraseña_sistema == Contraseña_usuario:
+        print("Contraseña correcta")
+    else:
+        print("contraseña incorrecta")        
+
+validacion_contraseña()
